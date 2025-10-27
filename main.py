55a6917d1f0eae77a7516cc6b8b049b3e0b8e216
Yuom Kitya <kityayuom@gmail.com>
2025-10-27 22:12:40 +0700

    add fastapi

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from uv-study!")
-
-
-if __name__ == "__main__":
-    main()
+from fastapi import FastAPI
+from typing import Union
+app = FastAPI()
+@app.get("/")
+def root():
+    return {"message": "Hello, World!"}
+#route parameter: /{item_id}
+# /router_namae/route_parameter?query = 
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
