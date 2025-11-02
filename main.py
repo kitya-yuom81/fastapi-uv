181f415b5b4b44467050903196c0cea1de9e5c17
Yuom Kitya <kityayuom@gmail.com>
2025-11-02 17:40:47 +0700

    rean tt

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from fastapi import FastAPI
-from typing import Union
+from typing import Optional  # same as Union[int, None] but shorter
+
 app = FastAPI()
+
+# GET /
 @app.get("/")
 def root():
-    return {"message": "Hello, World!"}
-#route parameter: /{item_id}
-# /router_namae/route_parameter?query = 
+    return {"message": "Hello World"}
+
+# GET /items/{item_id}?page=...&limit=...
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def read_item(
+    item_id: int,                 # path parameter
+    page: Optional[int] = None,   # optional query parameter
+    limit: Optional[int] = None,  # optional query parameter
+):
+    return {
+        "item_id": item_id,
+        "page": page,
+        "limit": limit,
+    }
+
+# (optional: run with `python main.py`)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
